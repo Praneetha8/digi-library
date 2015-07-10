db05ad6f5dd4ccf841650a0f2d4c13bca0a74cd8
Praneetha Sattiraju <Praneetha Sattiraju@ODC000110575001.ads.mckinsey.com>
2015-07-10 15:24:42 +0530

    First commit

---
+++ mylibrary/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mylibraryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mylibraryApp
+ */
+angular.module('mylibraryApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ mylibrary/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mylibraryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mylibraryApp
+ */
+angular.module('mylibraryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
