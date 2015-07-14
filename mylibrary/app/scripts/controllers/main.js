af869aa1238ea599d7d6b7e933c28eeb4dd962b5
unknown <Rasika Adawatkar@BNG000133789002.ads.mckinsey.com>
2015-07-14 19:30:27 +0530

    redesign login page

--- mylibrary/app/scripts/controllers/main.js
+++ mylibrary/app/scripts/controllers/main.js
@@ -8,10 +8,36 @@
  * Controller of the mylibraryApp
  */
 angular.module('mylibraryApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope,$mdDialog) {
+
+	$scope.showSignUpModal= function(ev) {
+		  $scope.alert = '';
+    $mdDialog.show({
+       controller: DialogController,	
+      templateUrl:"../../views/Directives/sign-up.html",
+      targetEvent: ev,
+    })
+     .then(function(answer) {
+      $scope.alert = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.alert = 'You cancelled the dialog.';
+    });
+  };
+
+ function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
+
   });
+
+  
---
+++ mylibrary/app/scripts/directives/sign-up.js
@@ -0,0 +1,13 @@
+angular.module('mylibraryApp').directive('signUp',function(){
+	return{
+		restrict: 'A',
+		replace:true,
+		scope:{
+
+		},
+		templateUrl:'../../views/Directives/sign-up.html',
+		link:function(){
+
+		}
+	}
+})
